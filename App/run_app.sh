c66239654adf9483e88b0b1089032ba15d020f27
James Brundage <jamesbrundage@jamess-mbp-2.uconnect.utah.edu.ucsf.edu>
2024-11-20 12:50:45 -0700

    testing example.sh with cloning repo and gitlfs

--- App/run_app.sh
+++ App/run_app.sh
@@ -1,15 +1,20 @@
 EXTRACT="$1"
 INFERENCE="$2"
 
-# pip install --no-cache-dir -r requirements.txt
+git clone https://github.com/James-Brundage/ECG_PASP.git
+cd ECG_PASP/
+
+git-lfs install
+git lfs fetch
+git lfs checkout
 
 # Extraction step
 if [ "${EXTRACT}" = "TRUE" ]; then
-    python /App/XML_Extraction/Extraction.py
+    python App/XML_Extraction/Extraction.py
 fi
 
 # Inference
 if [ "${INFERENCE}" = "TRUE" ]; then
-    chmod +x /App/XML_Extraction/xml2ecg_updatedleads.linux
-    python /App/Model_Inference/apply.py
+    chmod +x App/XML_Extraction/xml2ecg_updatedleads.linux
+    python App/Model_Inference/apply.py
 fi
